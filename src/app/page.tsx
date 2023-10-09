c599c91f30233a0d278363cfb53a1ea075a84b17
Ngoran Aristide <ngoranaristide4@gmail.com>
2023-10-09 21:12:36 +0100

    some updates.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ import showcase from '../../public/works.png'
 import signature from '../../public/signature.png'
 import { TwitterLogoIcon, LinkedInLogoIcon, GitHubLogoIcon, StarIcon } from '@radix-ui/react-icons'
 import jsnad from '../../public/jsnad.png'
+import icon2 from '../../public/icon2.png'
+import { NavMenu } from '@/components/NavigationMenu'
 
 export default function Home() {
   return (
@@ -251,15 +253,46 @@ export default function Home() {
       <section className='grid grid-cols-2 gap-5 h-64'>
         <div className='text-neutral-50 relative bg-zinc-800 grid grid-cols-3 gap-5 rounded-[30px] h-64 p-5 w-full bg-gradient-to-br from-zinc-800 to-neutral-900' >
           <Image src={bg} fill alt='bg_picture' priority className='rounded=[30px] overflow-clip opacity-10 ' sizes='100vw' style={{ objectFit: 'cover' }} />
-
-          <div className='rounded-[30px] h-full w-full flex flex-col items-center justify-center bg-gradient-to-br from-zinc-700 to-neutral-900' >heo</div>
-          <div className='bg-zinc-800 rounded-[30px] h-full w-full  flex flex-col items-center justify-center bg-gradient-to-br from-zinc-700 to-neutral-900' ></div>
-          <div className='bg-zinc-800 rounded-[30px] h-full w-full  flex flex-col items-center justify-center bg-gradient-to-br from-zinc-700 to-neutral-900' ></div>
+          <div className='rounded-[30px] h-full w-full flex gap-5 flex-col items-center justify-center bg-gradient-to-br from-zinc-700 to-neutral-900' >
+            <p className='text-3xl font-bold '>03</p>
+            <div className='uppercase text-neutral-500 text-center font-semibold text-xs'>
+              <p>YEARS</p> <p>EXPERIENCE</p>
+            </div>
+          </div>
+          <div className='rounded-[30px] h-full w-full flex gap-5 flex-col items-center justify-center bg-gradient-to-br from-zinc-700 to-neutral-900' >
+            <p className='text-3xl font-bold '>+2</p>
+            <div className='uppercase text-neutral-500 text-center text-xs font-semibold'>
+              <p>clients</p> <p>worldwide</p>
+            </div>
+          </div>
+          <div className='rounded-[30px] h-full w-full flex gap-5 flex-col items-center justify-center bg-gradient-to-br from-zinc-700 to-neutral-900' >
+            <p className='text-3xl font-bold '>+10</p>
+            <div className='uppercase text-neutral-500 text-xs text-center font-semibold'>
+              <p>Total </p> <p>Projects</p>
+            </div>
+          </div>
         </div>
-        <div className='bg-zinc-800 rounded-[30px] relative h-full w-full bg-gradient-to-br from-neutral-800 to-neutral-950' >
+        <div className='bg-zinc-800 rounded-[30px] relative h-full group w-full bg-gradient-to-br from-neutral-800 flex gap-5 flex-col  to-neutral-950' >
           <Image src={bg} fill alt='bg_picture' priority className='rounded=[30px] overflow-clip opacity-10 ' sizes='100vw' style={{ objectFit: 'cover' }} />
+          <Image src={icon2} alt='icon_image' style={{ position: 'relative', height: 'auto', maxWidth: '100%', top: 0, left: '30px' }} className='opacity-50' />
+          <p className='text-4xl font-bold text-neutral-50 p-5' >Let&apos;s <br />
+            <span className='flex items-center justify-between'>
+              <span>
+                work <span className='text-blue-600'>together.</span>
+              </span>
+              <svg className='group-hover:stroke-neutral-50 stroke-neutral-500 stroke-1' width="46" height="42" viewBox="0 0 46 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path className='group-hover:stroke-neutral-50 stroke-neutral-500 stroke-1' d="M30.9224 21.2014C25.1291 21.2014 24.618 24.7796 24.618 27.3354C24.618 21.5421 21.5509 21.2014 17.9727 21.2014C24.618 21.2014 24.618 17.2824 24.618 14.8969C24.618 20.3494 27.8554 21.2014 30.9224 21.2014Z" fill="#F4F4F4" stroke="#F4F4F4" />
+                <rect y="21.6981" width="0.999999" height="18" transform="rotate(-90 0 21.6981)" className='group-hover:stroke-neutral-50 stroke-neutral-500 stroke-1' fill="#F4F4F4" />
+                <path className='group-hover:stroke-neutral-50 stroke-neutral-500 stroke-1' d="M9.93715 16.8555C10.9514 13.0701 13.2829 9.77074 16.5123 7.55063C19.7417 5.33052 23.6571 4.33531 27.5547 4.74394C31.4522 5.15258 35.0762 6.93825 37.7749 9.77989C40.4736 12.6215 42.07 16.3327 42.2771 20.2461C42.4842 24.1596 41.2884 28.0185 38.9047 31.1291C36.5211 34.2398 33.1059 36.398 29.2732 37.2157C25.4406 38.0335 21.4419 37.4571 17.9962 35.5903C14.5505 33.7234 11.8839 30.6886 10.4757 27.0314" stroke="#F4F4F4" stroke-width="1.5" stroke-linecap="round" />
+              </svg>
+            </span>
+          </p>
         </div>
       </section>
+      {/* <section className='py-10 flex flex-col text-neutral-50  gap-5' > */}
+      {/*   <p className=' font-bold'> NgoranA</p> */}
+      {/* <NavMenu /> */}
+      {/* </section> */}
     </main>
   )
 }
